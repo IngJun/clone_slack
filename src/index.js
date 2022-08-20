f6a81ee04ee29f5ad87def801d50f3075ff4063e
IngJun <107452558+IngJun@users.noreply.github.com>
2022-08-21 01:34:17 +0900

    add ConfigStore & jwt auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from "./redux/ConfigStore";
+import { ConnectedRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import App from './shared/App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const history = createBrowserHistory();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>  
+      <ConnectedRouter history={history}>
+        <App /> 
+      </ConnectedRouter>
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
