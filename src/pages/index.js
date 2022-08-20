d75a6f07fc5f5bef38216cbb49d5ab91fb4ea34e
IngJun <107452558+IngJun@users.noreply.github.com>
2022-08-21 01:19:24 +0900

    page set up & add router setting

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import ChannelHome from "./ChannelHome";
+import Login from "./Login";
+import Channel from "./Channel"
+import signupPage from "./signuppage";
+
+export { ChannelHome, Channel, Login, signupPage };
